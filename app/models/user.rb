e387fc631c8f83bbe5338cac805b9da02eb1787f
Raghu Betina <raghu@firstdraft.com>
2018-03-15 04:52:07 +0000

    Direct association between users and mentorship_pairings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_one    :mentorship_pairing,
+             :foreign_key => "mentee_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
